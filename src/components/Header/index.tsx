29665960201955423b1fa2f0101aba82441e016f
Your Name <your-email@example.com>
2025-03-25 21:39:03 +0400

    add comment in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
 import Logo from "/images/logo-mark.svg";
 
 const Header = () => {
+  //header
   return (
+    
     <header className="flex h-32 w-full items-center justify-center gap-4">
       <img src={Logo} alt="Logo" />
       <p className="text-3xl font-bold">Coding Conf</p>
